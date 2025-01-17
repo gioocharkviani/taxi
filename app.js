23f63e3d50eb59f45485812d65a3330229d2846e
gioocharkviani <gioocharkviani@gmail.com>
2025-01-17 01:10:45 -0800

    finiish

---
+++ app.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const header = document.querySelector(".main-header");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
+});
+
+const burgerMenu = document.querySelector(".burger-menu");
+const menuOverlay = document.querySelector(".menu-overlay");
+
+burgerMenu.addEventListener("click", () => {
+  menuOverlay.classList.toggle("active");
+  burgerMenu.classList.toggle("open");
+});
+
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", async function (e) {
+    e.preventDefault();
+    const btn = document.getElementById("con-btn");
+
+    const formData = new FormData(e.target);
+
+    try {
+      btn.innerHTML = "იგზავნება...";
+      const response = await fetch(
+        "https://texi-two.vercel.app/send_email.php",
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+      if (response.ok) {
+        btn.innerHTML = "მეილი გაგზავნილია..";
+        btn.style.backgroundColor = "green";
+        btn.style.color = "white";
+        e.target.reset();
+      } else {
+        btn.innerHTML = "შეცდომა გაგზავნის დროს ";
+        btn.style.backgroundColor = "red";
+        btn.style.color = "white";
+      }
+      setTimeout(() => {
+        btn.innerHTML = "დაგვიკავშირდით";
+        btn.style.backgroundColor = "black";
+      }, 5000);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  });
---
+++ send_email.php
@@ -0,0 +1,51 @@
+<?php
+
+require 'src/Exception.php';
+require 'src/PHPMailer.php';
+require 'src/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $company_name = $_POST['company_name'];
+    $person = $_POST['person'];
+    $city = $_POST['city'];
+    $email = $_POST['email'];
+    $contact = $_POST['contact'];
+
+    // Email details
+    $to = "gioocharkviani@gmail.com";
+    $subject = "New Contact from $company_name";
+    $message = "Company Name: $company_name\nPerson: $person\nCity: $city\nEmail: $email\nContact: $contact";
+
+    // SMTP configuration
+    $mail = new PHPMailer(true);
+
+    try {
+
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'gioocharkviani@gmail.com'; // Your Gmail address
+        $mail->Password = 'ijtr mpyb dgym hcam';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; 
+        $mail->Port = 465;
+
+        // Sender and recipient settings
+        $mail->setFrom("$email" , "$company_name"); 
+        $mail->addAddress($to); 
+
+        // Email content
+        $mail->isHTML(false); 
+        $mail->Subject = $subject;
+        $mail->Body = $message;
+
+        $mail->send();
+        echo "Email sent successfully!";
+    } catch (Exception $e) {
+        http_response_code(500);
+        echo "Email sending failed! Error: {$mail->ErrorInfo}";
+    }
+}
+?>
